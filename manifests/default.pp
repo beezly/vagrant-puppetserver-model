00d333ea21f6bdf17c70ddaa7b032e20741da2f7
Andrew Beresford <beezly@beezly.org.uk>
2014-10-03 13:14:41 +0100

    Use localhost for the server
    Set dns_alt_name and server to localhost so that we can practice
    on ourselves

--- manifests/default.pp
+++ manifests/default.pp
@@ -14,10 +14,22 @@ node default {
     require => Package['puppetserver'],
   }
   
+  augeas {'puppet dns_alt_names':
+    context => '/files/etc/puppet/puppet.conf', 
+    changes => 'set main/dns_alt_names localhost',
+    require => Package['puppet'],
+  }
+  
+  augeas {'puppet server':
+    context => '/files/setc/puppet/puppet.conf',
+    changes => 'set main/server localhost',
+    require => Package['puppet'],
+  }
+  
   service {'puppetserver':
     ensure  => 'running',
     enable  => 'true',
-    require => [Package['puppetserver'],File['/var/run/puppet']],
+    require => [Package['puppetserver'],File['/var/run/puppet'],Augeas['puppet dns_alt_names']],
   }
   
 }
