a2b4decda2d98777b5c3df8b592fcbc00bf7f63f
Andrew Beresford <beezly@beezly.org.uk>
2014-10-03 12:44:44 +0100

    Workaround PUP-3035 - create /var/run/puppet before starting

--- manifests/default.pp
+++ manifests/default.pp
@@ -3,14 +3,21 @@ node default {
   include puppetlabs_apt
  
   package {['puppet','puppetserver']:
-    ensure => 'latest',
+    ensure  => 'latest',
     require => Class['puppetlabs_apt'],
   }
   
-  service {'puppetserver':
-    ensure => 'running',
-    enable => 'true',
+  file {'/var/run/puppet': 
+    ensure  => 'directory',
+    owner   => 'puppet',
+    group   => 'puppet'
     require => Package['puppetserver'],
+  }
+  
+  service {'puppetserver':
+    ensure  => 'running',
+    enable  => 'true',
+    require => [Package['puppetserver'],File['/var/run/puppet']],
   }
   
 }
